89fb5be4f311a2e45ba9037b907ba8b73d3f57e3
krjovan <jovankron@gmail.com>
2021-02-05 01:56:07 +0100

    Authnetification service created, designed responsive navbar and footer, added w3css and font-awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'E-residency-frontend';
+  title = 'E-residency';
+
+  constructor(public auth: AuthenticationService) {}
+
+  myFunction() {
+    const x = document.getElementById('myTopnav');
+    const navSignIn = document.getElementById('navSignIn');
+    const userDropdown = document.getElementById('userDropdown');
+
+    if (x.className === 'topnav') {
+        x.className += ' responsive';
+        if (navSignIn !== null) {
+          navSignIn.className = '';
+        }
+        if (userDropdown !== null) {
+          userDropdown.className = 'dropdown';
+        }
+    } else {
+        x.className = 'topnav';
+        if (navSignIn !== null) {
+          navSignIn.className = 'w3-right';
+        }
+        if (userDropdown !== null) {
+          userDropdown.className = 'dropdown w3-right';
+        }
+    }
+  }
+
+  toTop() {
+    document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  /*{ path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] },*/
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
